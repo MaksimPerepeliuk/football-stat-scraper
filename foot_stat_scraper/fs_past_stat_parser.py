1156ee16b5efd6b35db0fedfdb820adfe25a7e61
Max <maksim.perepelyuk@mail.ru>
2020-07-27 11:07:35 +0300

    implement function for collect stat from command result page

--- foot_stat_scraper/fs_past_stat_parser.py
+++ foot_stat_scraper/fs_past_stat_parser.py
@@ -3,32 +3,33 @@ from bs4 import BeautifulSoup
 from logs.loggers import app_logger
 
 
-def calculate_stat(stats): # неправильные значения
+def calculate_stat(stats):  # неправильные значения
     app_logger.debug('Start CALCULATING collected past stats')
     slices = [3, 5, 10, 15, 20]
     sums = {}
     slice_stats = {}
     for i, stat in enumerate(stats):
-        if i + 1 in slices:
-            keys = sums.keys()
-            values = sums.values()
-            [slice_stats.update({f'{i+1}_last_{key}': value})
-             for key, value in zip(keys, values)]
-            app_logger.info(f'Make SLICE stats last {i} previous events')
         keys = stat.keys()
         for key in keys:
             if key in sums:
                 sums[key] += int(stat[key])
             else:
                 sums[key] = int(stat[key])
+        if i + 1 in slices:
+            keys = sums.keys()
+            values = sums.values()
+            [slice_stats.update({f'{i+1}_last_{key}': value})
+             for key, value in zip(keys, values)]
+            app_logger.info(f'Make SLICE stats last {i+1} previous events')
     app_logger.debug(f'\n{slice_stats}')
+    return slice_stats
 
 
-def get_detail_stat(stat_rows, command, championate, limit=3):
-    app_logger.debug(f'Start calculate stats for {command}')
+def get_detail_stat(stat_rows, command, championate, limit=10):
+    app_logger.debug(f'Start received DETAIL stats for {command}')
     summary_stats = []
     for i, stat_row in enumerate(stat_rows):
-        if i > limit:
+        if i + 1 > limit:
             app_logger.info(f'Processed {i} events, exit from loop')
             break
         event_stat = {}
@@ -48,15 +49,20 @@ def get_detail_stat(stat_rows, command, championate, limit=3):
             event_id = stat_row['id'][4:]
             first_half_url = f'https://www.flashscore.com/match/{event_id}/#match-statistics;1'
             second_half_url = f'https://www.flashscore.com/match/{event_id}/#match-statistics;2'
+            app_logger.info(
+                '--------------------------------------------------------------------')
+            app_logger.info(f'STAT ROW {stat_row}')
+            app_logger.info(
+                f'DETAIL STAT {home_command} {event_scores} {away_command}')
             event_stat.update(get_half_stat(first_half_url, '1st_half'))
             event_stat.update(get_half_stat(second_half_url, '2nd_half'))
             summary_stats.append(event_stat)
             app_logger.debug(
-                f'Add event #{i} with {len(event_stat)} keys stat in summary stat')
+                f'Add event #{i+1} with {len(event_stat)} keys stat in summary stat')
         except Exception:
             app_logger.exception(
                 f'\nError received data from stat row {command}')
-    calculate_stat(summary_stats)
+    return(calculate_stat(summary_stats))
 
 
 def get_past_command_stat(url, command, championate, event_date):
@@ -74,8 +80,10 @@ def get_past_command_stat(url, command, championate, event_date):
         event_date_format = event_date_parts[0] + \
             event_date_parts[1] if len(date) > 1 else event_date
         if date_format == event_date_format:
+            app_logger.debug(
+                f'Element date {date_format} == event date {event_date_format}')
             prev_events = stat_row.find_next_siblings(
-                name='div', attrs={'class': 'event__match'})
+                name='div', attrs={'class': 'event__match event__match--static event__match--oneLine'})
             return get_detail_stat(prev_events, command, championate)
 
 
@@ -94,6 +102,7 @@ def get_summary_past_stat(url):
     home_past_stat = get_past_command_stat(
         home_command_url, main_stat['home_command'],
         main_stat['championate'], main_stat['date'])
+    print(111111111111111111, home_past_stat)
 
 
 def insert_past_stat(url):
